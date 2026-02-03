8c26e18fed5cbbfed56256f60d203ee3892b1ecc
VivekPrajapati-Git <vivekbusiness20@gmail.com>
2026-02-03 19:43:13 +0530

    Command for gmail authentication written

--- src/setfile/GoogleOAuth/auth_server.py
+++ src/setfile/GoogleOAuth/auth_server.py
@@ -1,35 +1,41 @@
 import os
 import pathlib
 import requests as r
-from flask import Flask, session, abort, redirect, request
+from flask import Flask, session, redirect, request
 from google_auth_oauthlib.flow import Flow
 from setfile.GoogleOAuth.db import get_db, init_db
+import threading
 
 app = Flask("Google-Login App")
 app.secret_key = "hello"
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
 
-client_secret_file = pathlib.Path(__file__).resolve().parent.parent.parent.parent/"secret/google_oauth.json"
+client_secret_file = pathlib.Path(__file__).resolve().parent.parent.parent.parent/"secret/gmail_auth.json"
 SCOPES = [
     'https://www.googleapis.com/auth/gmail.readonly',
     "openid",
     "https://www.googleapis.com/auth/userinfo.email",
 ]
 
-flow = Flow.from_client_secrets_file(
-    client_secret_file,
-    scopes = SCOPES,
-    redirect_uri = "http://localhost:5000/callback"
-)
-
 @app.route("/login")
 def login():
+    flow = Flow.from_client_secrets_file(
+        client_secret_file,
+        scopes = SCOPES,
+        redirect_uri = "http://localhost:5000/callback"
+    )
     auth_url, state = flow.authorization_url(prompt="consent")
     session["state"] = state
     return redirect(auth_url)
 
 @app.route("/callback")
 def callback():
+    flow = Flow.from_client_secrets_file(
+        client_secret_file,
+        scopes = SCOPES,
+        redirect_uri = "http://localhost:5000/callback"
+    )
+    
     flow.fetch_token(authorization_response=request.url)
 
     credentials = flow.credentials
@@ -42,16 +48,16 @@ def callback():
     google_id = user_info['id']
     email = user_info['email']
 
-    
+    expires_at = credentials.expiry.isoformat() if credentials.expiry else None
     db = get_db()
     cursor = db.cursor()
-    cursor.execute("INSERT OR REPLACE INTO users (google_id, email, access_token , refresh_token , expires_at) VALUES (?, ?, ?, ?, ?)", (google_id, email, credentials.token, credentials.refresh_token, credentials.expiry))
+    cursor.execute("INSERT OR REPLACE INTO users (google_id, email, access_token , refresh_token , expires_at) VALUES (?, ?, ?, ?, ?)", (google_id, email, credentials.token, credentials.refresh_token or "", expires_at))
     db.commit()
     db.close()
 
     path = pathlib.Path(__file__).resolve().parent.parent/'session.json'
     with open(path,'w') as f:
-        f.write(f' id : {user_info['id']} , email : {user_info['email']}')
+        f.write(f'{user_info}')
 
     return "Login successful! You can close this tab."
 
@@ -61,4 +67,4 @@ def index():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(port=5000)
--- src/setfile/cli.py
+++ src/setfile/cli.py
@@ -1,7 +1,10 @@
+import os
 import click
+import webbrowser
+import subprocess
+import time
 from src.setfile.commands.organize import organize_files
 from src.setfile.commands.revert import revert
-import os
 
 # Making one single command
 @click.group()
@@ -19,9 +22,14 @@ def revert():
     revert()
 
 @main.command()
-@click.option('--email', required=True)
-def gmail_auth(email):
-    pass
+def gmail_auth():
+    click.echo("Starting Gmail Authentication Server...")
+    subprocess.Popen(["python", "-m", "setfile.GoogleOAuth.auth_server"])
+    time.sleep(1.5)
+
+    webbrowser.open("http://localhost:5000/")
+    click.echo("Browser Opened for gmail connection..")
+    click.echo("After Connection you can close the browser!")
 
 @main.command()
 def gmail_org():
